82d88dc8fd8fdddf33fd13d50ecd0181f0b1781d
da0293 <dy7133@naver.com>
2023-08-23 13:09:37 +0900

    [feat] 이름,전화번호 컬럼추가 및 회원가입 조건 기능 추가

--- moashareProject/src/main/java/com/moashare/dto/MemberDTO.java
+++ moashareProject/src/main/java/com/moashare/dto/MemberDTO.java
@@ -10,15 +10,19 @@ public class MemberDTO {
 	
 	private String id; 
 	private String pw; 
+	private String name;
 	private String nickname;
+	private String tel;
 	private String auth;
 	private String join_dt;
 	
 	@Builder
-	public MemberDTO(String id, String pw, String nickname, String auth, String join_dt) {
+	public MemberDTO(String id, String pw, String name, String nickname, String tel, String auth, String join_dt) {
 		this.id=id; 
 		this.pw=pw;
+		this.name=name;
 		this.nickname=nickname;
+		this.tel=tel;
 		this.auth=auth;
 		this.join_dt=join_dt;
 	}
