62acd2c7c1020087d8a6dac0f4f1eb3a212ee4dd
da0293 <dy7133@naver.com>
2023-10-03 23:05:39 +0900

    [feature] notice테이블 생성 및 페이지단연결

--- moashareProject/src/main/java/com/moashare/controller/NoticeController.java
+++ moashareProject/src/main/java/com/moashare/controller/NoticeController.java
@@ -1,5 +1,25 @@
 package com.moashare.controller;
 
-public class NoticeController {
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.moashare.config.auth.PrincipalDetails;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class NoticeController {
+	
+	@GetMapping("/notice")
+	public String member(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+		log.info("컨트롤러");
+		model.addAttribute("nickname", principalDetails.getMember().getNickname());
+		model.addAttribute("nickname", principalDetails.getMember().getNickname());
+		return "notice/notice";
+	}
 }
---
+++ moashareProject/src/main/java/com/moashare/model/Notice.java
@@ -0,0 +1,47 @@
+package com.moashare.model;
+
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Notice {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="제목은 필수입력값입니다.")
+	@Size(min = 1, max = 100)
+	private String ntitle;
+	
+	@NotBlank(message="내용은 필수입력값입니다.")
+	@Column(columnDefinition = "LONGTEXT")
+	private String ncontent;
+	
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="member_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Member member;
+	
+	@CreationTimestamp
+	private Timestamp reg_dt;
+}
