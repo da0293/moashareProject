5847746512103a9d2d646d02b752a3f5a9a3faf6
da0293 <dy7133@naver.com>
2023-10-12 21:25:53 +0900

    [feature] 공지사항 목록 및 상세보기

--- moashareProject/src/main/java/com/moashare/controller/NoticeController.java
+++ moashareProject/src/main/java/com/moashare/controller/NoticeController.java
@@ -5,9 +5,13 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.moashare.config.auth.PrincipalDetails;
+import com.moashare.service.NoticeService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -15,12 +19,28 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class NoticeController {
 	
+	private final NoticeService noticeService;
+	
+	public NoticeController(NoticeService noticeService) {
+		this.noticeService = noticeService;
+	}
+	
 	@GetMapping("/notice")
 	public String notice(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
 		log.info("컨트롤러");
-		model.addAttribute("nickname", principalDetails.getMember().getNickname());
+		model.addAttribute("noticeList", noticeService.getNoticeList());
 		model.addAttribute("nickname", principalDetails.getMember().getNickname());
 		return "notice/notice";
+	}
+	
+	@GetMapping("/notice/{noticeId}")
+	public String findById(@PathVariable Long noticeId, Model model,
+			 HttpServletRequest req, HttpServletResponse res,
+			@AuthenticationPrincipal PrincipalDetails principalDetails) {
+		model.addAttribute("nickname", principalDetails.getMember().getNickname());
+		model.addAttribute("notice", noticeService.detailView(noticeId));
+		return "notice/detailNotice";
+		
 	}
 
 }
--- moashareProject/src/main/java/com/moashare/service/NoticeService.java
+++ moashareProject/src/main/java/com/moashare/service/NoticeService.java
@@ -1,9 +1,14 @@
 package com.moashare.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.moashare.model.Board;
 import com.moashare.model.Notice;
+import com.moashare.model.Reply;
 import com.moashare.repository.NoticeRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -20,5 +25,19 @@ public class NoticeService {
 	 public void saveNotice(Notice notice) {
 		noticeRepository.save(notice);
 	 }
+
+	public List<Notice> getNoticeList() {
+		List<Notice> noticeList=noticeRepository.findAll();
+		return noticeList;
+	}
+	
+	@Transactional
+	public Notice detailView(Long id) {
+		Notice notice=noticeRepository.findById(id)
+				.orElseThrow(() -> {
+					return new IllegalArgumentException("아이디를 찾을 수 없어 글을 볼 수 없습니다.");
+				});
+		return notice;
+	}
 	
 }
