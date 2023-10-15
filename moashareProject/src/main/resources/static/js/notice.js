ba9de78beadef6e9705bcd66a608c6cd071da199
da0293 <dy7133@naver.com>
2023-10-16 01:21:39 +0900

    [chore] 알림창 2번뜨는것 방지

--- moashareProject/src/main/resources/static/js/notice.js
+++ moashareProject/src/main/resources/static/js/notice.js
@@ -34,42 +34,6 @@ $(document).on('click', '#btnNoticeSave', function(e) {
 });
 
 
-
-
-$(document).on('click', '#btn-reply-content', function(e) {
-
-	e.preventDefault();
-	console.log("버튼눌림");
-	const rcontent = $("#reply-content").val();
-	const board_id = $("#boardId").val();
-	const member_id = $("#memberId").val();
-
-	const params = {
-		member_id: member_id,
-		board_id: board_id,
-		rcontent: rcontent,
-	};
-
-	// ajax 호출 시 default가 비동기 호출, 아래 코드 실행 가능
-	$.ajax({
-		url: '../../boardApi/' + board_id + '/reply',
-		type: 'post',
-		contentType: 'application/json; charset=utf-8',
-		data: JSON.stringify(params),
-		dataType: 'json',
-		async: false,
-		success: function(response) {
-			console.log(response);
-			console.log(board_id);
-			alert("댓글 작성이 완료되었습니다.");
-			$.ajax(getReplyList(board_id));
-			$('#reply-content').val('');
-		},
-		error: function() {
-			alert("댓글은 필수 입력값이며 최대 200자끼지 가능합니다.");
-		}
-	})
-});
 $(document).on('click', '#deleteNotice', function(e) {
 
 	e.preventDefault();
