ead08bf8276e8888ba8edbb82b8a90da0a9151f2
da0293 <dy7133@naver.com>
2023-10-16 02:10:09 +0900

    [fix] 매일 자정 @scheduled 실행, 캐싱처리 에러방지, sql문 조회수 150초과로 변경

--- moashareProject/src/main/java/com/moashare/enumpackage/CacheType.java
+++ moashareProject/src/main/java/com/moashare/enumpackage/CacheType.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum CacheType {
     CACHE_STORE("bookmarkCacheStore", 2 * 60, 50000), // 캐시 이름, 만료 시간(2뷴), 최대 횟수
-	CACHE_HotBoard("hotboardCacheStore", 10080 * 60, 250000); // 캐시 이름, 만료 시간(1주일), 최대 횟수 
+	CACHE_HotBoard("hotboardCacheStore", 1440 * 60, 250000); // 캐시 이름, 만료 시간(하루), 최대 횟수 
     private final String cacheName;     // 캐시 이름
     private final int expireAfterWrite; // 만료시간
     private final int maximumSize;      // 최대 갯수
--- moashareProject/src/main/java/com/moashare/service/BoardService.java
+++ moashareProject/src/main/java/com/moashare/service/BoardService.java
@@ -36,65 +36,62 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class BoardService {
 
-	 private final BoardRepository boardRepository;
-	 private final ReplyRepository replyRepository;
-	 private final MemberRepository memberRepository;
-	 private final String VIEWCOOKIENAME = "alreadyViewCookie";
-	 private final BookmarkRepository bookmarkRepository;
-	 private final HotBoardRepository hotBoardRepository;
-	 
-	 // 게시판 글 저장
-	 @Transactional // 함수 종료 시 자동 commit
-	 public void saveBoard(Board board) {
-		 boardRepository.save(board);
-	 }
-	 
+	private final BoardRepository boardRepository;
+	private final ReplyRepository replyRepository;
+	private final MemberRepository memberRepository;
+	private final String VIEWCOOKIENAME = "alreadyViewCookie";
+	private final BookmarkRepository bookmarkRepository;
+	private final HotBoardRepository hotBoardRepository;
+
+	// 게시판 글 저장
+	@Transactional // 함수 종료 시 자동 commit
+	public void saveBoard(Board board) {
+		boardRepository.save(board);
+	}
+
 	// 게시판 목록 가져오기
-	 
+
 	@Transactional(readOnly = true)
 	public Page<BoardDTO> boardList(Pageable pageable) {
-		Page<Board> boards= boardRepository.findAll(pageable);
-		List<BoardDTO> boardDTO=doPaging(boards);
+		Page<Board> boards = boardRepository.findAll(pageable);
+		List<BoardDTO> boardDTO = doPaging(boards);
 		return new PageImpl<>(boardDTO, pageable, boards.getTotalElements());
-		
+
 	}
 
 	// 게시판 목록(검색 시)가져오기
 	@Transactional(readOnly = true)
 	public Page<BoardDTO> boardSearchList(String searchKeyWord, Pageable pageable) {
-		Page<Board> boards= boardRepository.findByTitleContaining(searchKeyWord,pageable);
-		List<BoardDTO> boardDTO=doPaging(boards);
+		Page<Board> boards = boardRepository.findByTitleContaining(searchKeyWord, pageable);
+		List<BoardDTO> boardDTO = doPaging(boards);
 		return new PageImpl<>(boardDTO, pageable, boards.getTotalElements());
 	}
-	
+
 	// 공통 페이징 처리
 	private List<BoardDTO> doPaging(Page<Board> boards) {
-		List<BoardDTO> boardDTO=new ArrayList<>();
-		for(Board board : boards ) {
-			 BoardDTO result = BoardDTO.builder()
-					 .board(board)
-					 .build();
-			 boardDTO.add(result);
+		List<BoardDTO> boardDTO = new ArrayList<>();
+		for (Board board : boards) {
+			BoardDTO result = BoardDTO.builder().board(board).build();
+			boardDTO.add(result);
 		}
 		return boardDTO;
 	}
 
-	
 	// 게시판 상세페이지
 	@Transactional
 	public Board detailView(Long id) {
-		Board board=boardRepository.findById(id)
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디를 찾을 수 없어 글을 볼 수 없습니다.");
-				});
+		Board board = boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디를 찾을 수 없어 글을 볼 수 없습니다.");
+		});
 		return board;
 	}
-	
+
 	// 게시판 삭제
 	@Transactional
 	public void deleteBoard(Long id) {
@@ -104,193 +101,179 @@ public class BoardService {
 	// 게시판 수정
 	@Transactional
 	public void updateBoard(Long id, Board requestBoard) {
-		Board board=boardRepository.findById(id)
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디가 없어 글 찾기를 실패하였씁니다.");
-				}); // 영속화 완료
-		board.update(requestBoard.getTitle(),requestBoard.getContent());
+		Board board = boardRepository.findById(id).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 없어 글 찾기를 실패하였씁니다.");
+		}); // 영속화 완료
+		board.update(requestBoard.getTitle(), requestBoard.getContent());
 
 		// 해당 함수 종료시(Service가 종료될 때) 트랜잭션 종료, 이 때 더티체킹함(자동 업데이트)
 	}
 
-
-	
 	// 조회수 증가
 	public int updateView(Long id, HttpServletRequest request, HttpServletResponse response) {
 		Cookie[] cookies = request.getCookies();
-        boolean checkCookie = false;
-        int result = 0;
-        if(cookies != null){
-            for (Cookie cookie : cookies)
-            {
-                // 이미 조회를 한 경우 체크
-                if (cookie.getName().equals(VIEWCOOKIENAME+id)) checkCookie = true;
+		boolean checkCookie = false;
+		int result = 0;
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				// 이미 조회를 한 경우 체크
+				if (cookie.getName().equals(VIEWCOOKIENAME + id))
+					checkCookie = true;
 
-            }
-            if(!checkCookie){
-                Cookie newCookie = createCookieForForNotOverlap(id);
-                response.addCookie(newCookie);
-                result = boardRepository.updateHits(id);
-            }
-        } else {
-            Cookie newCookie = createCookieForForNotOverlap(id);
-            response.addCookie(newCookie);
-            result = boardRepository.updateHits(id);
-        }
-        return result;
+			}
+			if (!checkCookie) {
+				Cookie newCookie = createCookieForForNotOverlap(id);
+				response.addCookie(newCookie);
+				result = boardRepository.updateHits(id);
+			}
+		} else {
+			Cookie newCookie = createCookieForForNotOverlap(id);
+			response.addCookie(newCookie);
+			result = boardRepository.updateHits(id);
+		}
+		return result;
 	}
 
 	/*
-    * 조회수 중복 방지를 위한 쿠키 생성 메소드
-    * @param cookie
-    * @return
-    * 
+	 * 조회수 중복 방지를 위한 쿠키 생성 메소드
+	 * 
+	 * @param cookie
+	 * 
+	 * @return
+	 * 
 	 */
 	private Cookie createCookieForForNotOverlap(Long id) {
-		 Cookie cookie = new Cookie(VIEWCOOKIENAME+id, String.valueOf(id));
-	        cookie.setComment("조회수 중복 증가 방지 쿠키");	// 쿠키 용도 설명 기재
-	        cookie.setMaxAge(getRemainSecondForTommorow()); 	// 하루를 준다.
-	        cookie.setHttpOnly(true);				// 서버에서만 조작 가능
-	        return cookie;
+		Cookie cookie = new Cookie(VIEWCOOKIENAME + id, String.valueOf(id));
+		cookie.setComment("조회수 중복 증가 방지 쿠키"); // 쿠키 용도 설명 기재
+		cookie.setMaxAge(getRemainSecondForTommorow()); // 하루를 준다.
+		cookie.setHttpOnly(true); // 서버에서만 조작 가능
+		return cookie;
 	}
 
 	// 다음 날 정각까지 남은 시간(초)
-    private int getRemainSecondForTommorow() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime tommorow = LocalDateTime.now().plusDays(1L).truncatedTo(ChronoUnit.DAYS);
-        return (int) now.until(tommorow, ChronoUnit.SECONDS);
-    }
-    
-    // 북마크 기능
-    @CacheEvict(cacheNames = "bookmarkCacheStore", key="#memberId")
-    @Transactional
-    public String likeBoard(Long boardId, Long memberId ) {
-    	Board board=boardRepository.findById(boardId).orElseThrow(() -> {
-    		return new IllegalArgumentException("게시판이 제대로 확인되지않아 북마크에 실패하였씁니다.");
-    	});
-    	
-    	Member member=memberRepository.findById(memberId).orElseThrow(() -> {
-    		return new IllegalArgumentException("아이디가 제대로 확인되지않아 북마크에 실패하였씁니다.");
-    	});
-    	String bookmarkStatus="n"; 
-    	if(bookmarkRepository.findByBoardAndMember(board, member)==null) {
-    		// 북마크를 누른 적이 없다면 Bookmark 생성
-    		Bookmark bookmark = Bookmark.builder()
-    				.board(board)
-    				.member(member)
-    				.build();
-    		bookmarkRepository.save(bookmark);
-    		bookmarkStatus="y"; 
-    	} else {
-    		// 북마크 취소 후 테이블 삭제 
-    		Bookmark bookmark = bookmarkRepository.findByBoardAndMember(board, member);
-    		bookmark.unLikedBookmark(board);
-    		bookmarkRepository.delete(bookmark);
-    	}
-    	return bookmarkStatus;
-    	
-    }
-    // 로그인 아이디에 대한 북마크 가져오기 
+	private int getRemainSecondForTommorow() {
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime tommorow = LocalDateTime.now().plusDays(1L).truncatedTo(ChronoUnit.DAYS);
+		return (int) now.until(tommorow, ChronoUnit.SECONDS);
+	}
 
-    @Transactional(readOnly = true)
+	// 북마크 기능
+	@CacheEvict(cacheNames = "bookmarkCacheStore", key = "#memberId")
+	@Transactional
+	public String likeBoard(Long boardId, Long memberId) {
+		Board board = boardRepository.findById(boardId).orElseThrow(() -> {
+			return new IllegalArgumentException("게시판이 제대로 확인되지않아 북마크에 실패하였씁니다.");
+		});
+
+		Member member = memberRepository.findById(memberId).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 제대로 확인되지않아 북마크에 실패하였씁니다.");
+		});
+		String bookmarkStatus = "n";
+		if (bookmarkRepository.findByBoardAndMember(board, member) == null) {
+			// 북마크를 누른 적이 없다면 Bookmark 생성
+			Bookmark bookmark = Bookmark.builder().board(board).member(member).build();
+			bookmarkRepository.save(bookmark);
+			bookmarkStatus = "y";
+		} else {
+			// 북마크 취소 후 테이블 삭제
+			Bookmark bookmark = bookmarkRepository.findByBoardAndMember(board, member);
+			bookmark.unLikedBookmark(board);
+			bookmarkRepository.delete(bookmark);
+		}
+		return bookmarkStatus;
+
+	}
+	// 로그인 아이디에 대한 북마크 가져오기
+
+	@Transactional(readOnly = true)
 	public List<Bookmark> bookmarkList(Long memberId) {
-		Member member=memberRepository.findById(memberId).orElseThrow(() -> {
-    		return new IllegalArgumentException("아이디가 제대로 확인되지않아 북마크리스트를 가져오지못하였습니다.");
-    	});
-		
-		List<Bookmark> bookmarkList=bookmarkRepository.findByMember(member);
-		if (bookmarkList !=null) {
+		Member member = memberRepository.findById(memberId).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 제대로 확인되지않아 북마크리스트를 가져오지못하였습니다.");
+		});
+
+		List<Bookmark> bookmarkList = bookmarkRepository.findByMember(member);
+		if (bookmarkList != null) {
 			return bookmarkList;
 		}
 		return null;
 	}
-    
-    // 북마크체크된 북마크리스트 가져오기
-    @Cacheable(cacheNames = "bookmarkCacheStore", key="#memberId")
-    @Transactional(readOnly = true)
+
+	// 북마크체크된 북마크리스트 가져오기
+	@Cacheable(cacheNames = "bookmarkCacheStore", key = "#memberId")
+	@Transactional(readOnly = true)
 	public Page<BookmarkDTO> bookmarkCkList(Long memberId, Pageable pageable) {
-    	Member member=memberRepository.findById(memberId).orElseThrow(() -> {
-    		return new IllegalArgumentException("아이디가 확인되지않아 북마크를 가져오지못하였습니다.");
-    	});
-		Page<Bookmark> bookmarks= bookmarkRepository.findByMember(member, pageable);
-		int size=bookmarks.getSize();
-		List<BookmarkDTO> bookmarkDTO=doBookmarkPaging(bookmarks);
+		Member member = memberRepository.findById(memberId).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 확인되지않아 북마크를 가져오지못하였습니다.");
+		});
+		Page<Bookmark> bookmarks = bookmarkRepository.findByMember(member, pageable);
+		int size = bookmarks.getSize();
+		List<BookmarkDTO> bookmarkDTO = doBookmarkPaging(bookmarks);
 		return new PageImpl<>(bookmarkDTO, pageable, bookmarks.getTotalElements());
 	}
-    // 북마크 페이징처리
+
+	// 북마크 페이징처리
 	private List<BookmarkDTO> doBookmarkPaging(Page<Bookmark> bookmarks) {
-		List<BookmarkDTO> bookmarkDTO=new ArrayList<>();
-		for(Bookmark bookmark : bookmarks ) {
-			 BookmarkDTO result = BookmarkDTO.builder()
-					 .board(bookmark.getBoard())
-					 .build();
-			 bookmarkDTO.add(result);
+		List<BookmarkDTO> bookmarkDTO = new ArrayList<>();
+		for (Bookmark bookmark : bookmarks) {
+			BookmarkDTO result = BookmarkDTO.builder().board(bookmark.getBoard()).build();
+			bookmarkDTO.add(result);
 		}
 		return bookmarkDTO;
 	}
-	
+
 	// 댓글 작성
 	@Transactional
 	public void saveReply(ReplyDTO replyDTO) {
-		Member meber=memberRepository.findById(replyDTO.getMember_id())
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
-				}); // 영속화 완료
-		Board board=boardRepository.findById(replyDTO.getBoard_id())
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
-				}); // 영속화 완료
-		Reply reply=Reply.builder()
-				.rcontent(replyDTO.getRcontent())
-				.board(board)
-				.mebmer(meber)
-				.build();
+		Member meber = memberRepository.findById(replyDTO.getMember_id()).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
+		}); // 영속화 완료
+		Board board = boardRepository.findById(replyDTO.getBoard_id()).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
+		}); // 영속화 완료
+		Reply reply = Reply.builder().rcontent(replyDTO.getRcontent()).board(board).mebmer(meber).build();
 		replyRepository.save(reply);
 		board.increaseReplyCnt(board.getReplycnt());
 	}
 
-
-    // 댓글 수정
-    @Transactional
+	// 댓글 수정
+	@Transactional
 	public void updateReply(Long replyId, ReplyDTO replyDTO) {
-		Reply reply=replyRepository.findById(replyId)
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디가 없어 글 찾기를 실패하였씁니다.");
-				}); // 영속화 완료
+		Reply reply = replyRepository.findById(replyId).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 없어 글 찾기를 실패하였씁니다.");
+		}); // 영속화 완료
 		reply.update(replyDTO.getRcontent());
 	}
-    
+
 	// 댓글삭제
 	@Transactional
 	public void deleteReply(Long replyId) {
-		Reply reply =replyRepository.findById(replyId)
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
-				});
+		Reply reply = replyRepository.findById(replyId).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
+		});
 		replyRepository.deleteById(replyId);
-		 // 영속화 완료
-		Board board=boardRepository.findById(reply.getBoard().getId())
-				.orElseThrow(() -> {
-					return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
-				});
+		// 영속화 완료
+		Board board = boardRepository.findById(reply.getBoard().getId()).orElseThrow(() -> {
+			return new IllegalArgumentException("아이디가 없어 댓글 개수 찾기를 실패하였씁니다.");
+		});
 		board.decreaseReplyCnt(board.getReplycnt());
-		
+
 	}
-    // 댓글 리스트 가져오기 
-    @Transactional(readOnly = true)
-	public List<Reply> getReplyList(Long board_id){
-		Board board=boardRepository.findById(board_id).orElseThrow(() -> {
-    		return new IllegalArgumentException("게시글이 제대로 확인되지않아 댓글리스트를 가져오지못하였습니다.");
-    	});
-		List<Reply> replyList=new ArrayList<>();
-		replyList=replyRepository.findAllByBoardId(board_id);
-		if (replyList !=null) {
+
+	// 댓글 리스트 가져오기
+	@Transactional(readOnly = true)
+	public List<Reply> getReplyList(Long board_id) {
+		Board board = boardRepository.findById(board_id).orElseThrow(() -> {
+			return new IllegalArgumentException("게시글이 제대로 확인되지않아 댓글리스트를 가져오지못하였습니다.");
+		});
+		List<Reply> replyList = new ArrayList<>();
+		replyList = replyRepository.findAllByBoardId(board_id);
+		if (replyList != null) {
 			return replyList;
 		}
 		return null;
 
 	}
-    // 인기글 가져오기
+	// 인기글 가져오기
 
 //    @Transactional(readOnly = true)
 //	public List<Board> hotBoardList() {
@@ -298,32 +281,36 @@ public class BoardService {
 //		return hotBoardList;
 //	}
 //    
-    @Scheduled(cron = "0 0 0 * * *") // 매일 자정마다 실행
-    @Transactional
-   	public void hotBoardList() {
-    	List<Board> boardList = boardRepository.findAllByHotBoard();
-    	for (Board board : boardList) {
-    	    // board_id가 이미 존재하는지 확인
-    	    if (!hotBoardRepository.existsByBoardId(board.getId())) {
-    	        HotBoard hotBoard = HotBoard.builder()
-    	            .board(board)
-    	            .build();
-    	        log.info(hotBoard.getTitle());
-    	        hotBoardRepository.save(hotBoard);
-    	    }
-    	}
-   	}
-    
-    @Transactional
-    @CacheEvict(value = "hotboardCacheStore", allEntries = true)
-    @Scheduled(cron = "59 59 23 * * 7") // 매주 일요일 23시 59분 59초에 실행
-    public void autoDelete() {
-    	hotBoardRepository.deleteAllInBatch();
-    }
-
-    @Cacheable(cacheNames = "hotboardCacheStore") // 1주일 캐싱 
+	@Scheduled(cron = "0 0 0 * * *") // 매일 자정마다 실행
+	@Transactional
+	public void hotBoardList() {
+		List<Board> boardList = boardRepository.findAllByHotBoard();
+		autoDelete(); 
+		for (Board board : boardList) {
+			if (!hotBoardRepository.existsByBoardId(board.getId())) { // board_id가 이미 존재하는지 확인(에러 방지)
+				HotBoard hotBoard = HotBoard.builder().board(board).build();
+				log.info(hotBoard.getTitle());
+				hotBoardRepository.save(hotBoard);
+			}
+		}
+		clearHotBoardCache(); 
+	}
+	
+	// hotBoard 테이블에 있는 데이터 삭제
+	private void autoDelete() {
+		hotBoardRepository.deleteAllInBatch();
+	}
+	
+	// 캐싱 지우기
+	@CacheEvict(cacheNames = "hotboardCacheStore", allEntries = true)
+	private void clearHotBoardCache() {
+		
+	}
+	
+	// 주간 인기글(7일이내)하루 캐싱
+	@Cacheable(cacheNames = "hotboardCacheStore") 
 	public List<HotBoard> getHotBoardList() {
-		List<HotBoard> hotBoardList=hotBoardRepository.findAll();
+		List<HotBoard> hotBoardList = hotBoardRepository.findAll();
 		return hotBoardList;
 	}
 }
